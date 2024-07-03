48b6b9d8513e602c6ffe28140358aad9bc90c3a3
ExcellenceJnr <arikpostanley123@gmail.com>
2024-07-03 03:14:26 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Table from './views/Table'
+import Navbar from './views/Navbar'
+
+function App() {
+
+  return (
+    <div className='bg-gray-200'>
+    <Navbar />
+    <Table />
+    </div>
+  )
+}
+
+export default App
